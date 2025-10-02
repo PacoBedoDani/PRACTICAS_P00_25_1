eb0050cedf98a7af8072b82fecab689bb24f7dba
PacoBedoDani <85259065+PacoBedoDani@users.noreply.github.com>
2025-10-01 23:09:35 -0600

    MEJORA DEL CODIGO CONTROL BOLA
    SE MEJORO EL SCRIPT DEL CONTROL BOLA DEL BOLICHE CON NUEVAS VARIABLES, CONTROLADORES PARA EL OBJETO MEDIANTE METODOS, PARA FACILITAR MEJOR EL MANEJO DEL OBJETO Y TENER UNA MEJOR EXPERIENCIA.

--- Assets/Practica 1/Scripts/ControlBola.cs
+++ Assets/Practica 1/Scripts/ControlBola.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class ControlBola : MonoBehaviour
 {
+    public Transform CamaraPrincipal;
+    // variables que controlan al objeto bola
     public Rigidbody rb;
+    //variables para apuntar
+    public float velocidadDeApuntado = 5f;
+    public float limiteIzquierdo = -2f;
+    public float limiteDerecho = 2f;
+
     public float fuerzaDeLanzamiento = 1000f;
+    //se mejoro el control de flujo de la bola con un bool
+    private bool haSidoLanzada = false;
 
     // Start is called before the first frame update
     void Start()
@@ -15,10 +24,46 @@ public class ControlBola : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.AddForce(Vector3.forward * fuerzaDeLanzamiento);
+    { //Expresion: muestra que ha sido lanzada si es falso puede disparar
+        if (haSidoLanzada ==false)
+        {//se invoca el metodo de apuntar
+            Apuntar();
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //se cambia el valor cuando ya se lanzo la bola
+                //se utiliza o invoca el metodo lanzar
+                Lanzar();
+            }
         }
     }
-}
+
+    //1. Metodo o identificador Apuntar (leer un inputhorizotal de tipo axis te permite registrar
+    //entradas con las teclas A y D, y flecha izquierda y flecha derecha
+    void Apuntar()
+    {
+        float inputHorizontal = Input.GetAxis("Horizontal");
+
+        //2. Mover la bola hacia los lados
+        transform.Translate(Vector3.right * inputHorizontal * velocidadDeApuntado * Time.deltaTime);
+
+        //3. Delimitar el movimiento de la bola
+        //trasnform position me permite saber cual es la posicion actual de la bola dentro de la escena
+        Vector3 posicionActual = transform.position;
+
+        posicionActual.x = Mathf.Clamp(posicionActual.x, limiteIzquierdo, limiteDerecho);
+
+        transform.position = posicionActual;
+    }
+
+    //Nuevo metodo o identificador (void, verbo, llaves)
+    void Lanzar()
+    {
+        haSidoLanzada = true;
+        rb.AddForce(Vector3.forward * fuerzaDeLanzamiento);
+
+        if (CamaraPrincipal != null)
+        {
+            CamaraPrincipal.SetParent(transform);
+        }
+    }
+} //BIEN BENIDO A LA ENTRADA AL INFIERNO
